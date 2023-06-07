f096313878573bd20f87ace0da497f9cf91cd0b7
landryx <landryx@gmail.com>
2023-06-07 21:49:36 +0200

    Weather app - stage 2/5

--- Weather App/task/test/tests.py
+++ Weather App/task/test/tests.py
@@ -26,24 +26,72 @@ class FlaskProjectTest(FlaskTest):
             await browser.close()
         except Exception as ex:
             print(ex)
-            pass
 
-    async def test_main_page_structure(self):
+    async def test_response_async(self):
+        browser = await self.launch_and_get_browser()
+        page = await browser.newPage()
+        try:
+            await page.goto(self.get_url())
+        except Exception:
+            raise WrongAnswer(f"Can't access the main page with URL '{self.get_url()}'")
+        await self.close_browser(browser)
+
+    @dynamic_test(order=1, time_limit=-1)
+    def test_response(self):
+        ExitHandler.revert_exit()
+        asyncio.new_event_loop().run_until_complete(self.test_response_async())
+        return CheckResult.correct()
+
+    async def test_main_page_structure_async(self):
         browser = await self.launch_and_get_browser()
         page = await browser.newPage()
 
         await page.goto(self.get_url())
-        html_code = await page.content()
 
-        if "Hello, world!" not in html_code:
-            raise WrongAnswer("'/' route should return 'Hello, world!' message!")
+        cards_div = await page.querySelector('div.cards')
+
+        if cards_div is None:
+            raise WrongAnswer("Can't find <div> block with class 'cards'")
+
+        cards = await page.querySelectorAll('div.card')
+
+        if len(cards) == 0:
+            raise WrongAnswer("Can't find <div> blocks with class 'card'")
+
+        if len(cards) != 3:
+            raise WrongAnswer(f"Found {len(cards)} <div> blocks with class 'card', but should be 3!")
+
+        for card in cards:
+            degrees = await card.querySelector('div.degrees')
+            if degrees is None:
+                raise WrongAnswer(
+                    "One of the <div> blocks with card class 'card' doesn't contain <div> block with class 'degrees'")
+            state = await card.querySelector('div.state')
+            if state is None:
+                raise WrongAnswer(
+                    "One of the <div> blocks with card class 'card' doesn't contain <div> block with class 'state'")
+            city = await card.querySelector('div.city')
+            if city is None:
+                raise WrongAnswer(
+                    "One of the <div> blocks with card class 'card' doesn't contain <div> block with class 'city'")
+
+        input_field = await page.querySelector('input#input-city')
+
+        if input_field is None:
+            raise WrongAnswer("Can't find <input> element with id 'input-city'")
+
+        button = await page.querySelector('button.submit-button')
+
+        if button is None:
+            raise WrongAnswer("Can't find <button> element with class 'submit-button'")
 
         await self.close_browser(browser)
 
-    @dynamic_test(time_limit=-1)
-    def test(self):
-        ExitHandler.revert_exit()
-        asyncio.new_event_loop().run_until_complete(self.test_main_page_structure())
+        return CheckResult.correct()
+
+    @dynamic_test(order=2)
+    def test_main_page_structure(self):
+        asyncio.new_event_loop().run_until_complete(self.test_main_page_structure_async())
         return CheckResult.correct()
 
 
--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import sys
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    return render_template('index.html')
 
 
 # don't change the following way to run flask:
